8fed9f2664b580809f7c5a752bf85d889a8e4317
dosix <dosix@khu.ac.kr>
2025-06-06 17:02:09 +0900

    Fix blog URLs

--- settings.py
+++ settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-t1w#h9$-+*ymxe%^an&-b+-73(0!z#z2b()z*w3$rf^+qyb6$s
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['http://127.0.0.1:8001/', 'localhost', 'dosix.pythonanywhere.com']
 
 
 # Application definition
